a39236bb09802e3338635c272226d4186fff7165
VDVichev21 <VDVichev21@codingburgas.bg>
2022-11-27 11:40:47 +0200

    Implement main menu

---
+++ The game/the game/the game/the game.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+void Grid()
+{
+    
+}
+
+void writeInFile()
+{
+    fstream file;
+    file.open("levelEasy.txt", ios::out);
+    if (file.is_open()) {
+        file << "hat" << endl;
+        file << "act" << endl;
+        file << "cat" << endl;
+        file << "dog" << endl;
+        file << "bug" << endl;
+        file.close();
+    }
+}
+void readInFile()
+{
+    fstream file;
+    file.open("levelEasy.txt", ios::in);
+    if(file.is_open())
+    {
+        string temp;
+        do
+        {
+            cout << temp << endl;
+        } 
+        while (getline(file, temp));
+        file.close();
+    }
+}
+int main()
+{
+    writeInFile();
+    readInFile();
+    Grid();
+}
