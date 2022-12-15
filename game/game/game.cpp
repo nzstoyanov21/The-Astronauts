b4cfa246e629017089f8ab6d6e957f4bce18331c
VDVichev21@codingburgas.bg <VDVichev21@codingburgas.bg>
2022-12-15 09:39:51 +0200

    Fix Easy mode bug

--- game/game/game.cpp
+++ game/game/game.cpp
@@ -39,6 +39,9 @@ char grid[13][27] =
 
 void gameForEasyMode()
 {
+    counter = 0;
+    x = 2;
+    y = 1;
     string randomEasyWord;
     string fileEasy[5];
     string word;
@@ -56,22 +59,21 @@ void gameForEasyMode()
     {
         exit(1);
     }
-    
     randomEasyWord = fileEasy[rand() % 5];
     fileEasy[rand() % 5] = word;
     //cout << randomEasyWord[0] << " " << randomEasyWord[1] << " " << randomEasyWord[2] << endl;
     wordEasy.close();
     //save the coordinates about the letters
     int createWordEasy1Y = { 1 + rand() % 10 };
-    int createWordEasy1X = { 1 + rand() % 25 };
+    int createWordEasy1X = { 1 + rand() % 24 };
     grid[createWordEasy1Y][createWordEasy1X] = randomEasyWord[0];
 
     int createWordEasy2Y = { 1 + rand() % 10 };
-    int createWordEasy2X = { 1 + rand() % 25 };
+    int createWordEasy2X = { 1 + rand() % 24 };
     grid[createWordEasy2Y][createWordEasy2X] = randomEasyWord[1];
 
     int createWordEasy3Y = { 1 + rand() % 10 };
-    int createWordEasy3X = { 1 + rand() % 25 };
+    int createWordEasy3X = { 1 + rand() % 24 };
     grid[createWordEasy3Y][createWordEasy3X] = randomEasyWord[2];
 
     grid[createWordEasy1Y][createWordEasy1X] = randomEasyWord[0];
@@ -81,7 +83,7 @@ void gameForEasyMode()
     while(true)
     {    
 
-        if (y > 0 && y < 12 && x > 0 && x < 23)
+        if (y > 0 && y < 14 && x > 0 && x < 24)
         { 
             system("cls");
             for (int map = 0; map < 13; map++)
@@ -136,17 +138,28 @@ void gameForEasyMode()
             if(x == createWordEasy1X && y == createWordEasy1Y)
             {
                 counter++;
+                createWordEasy1X = 100;
             }
 
             if(x == createWordEasy2X && y == createWordEasy2Y)
             {
                 counter++;
+                createWordEasy2X = 100;
             }
             if (x == createWordEasy3X && y == createWordEasy3Y)
             {
                 counter++;
+                createWordEasy3X = 100;
             }
-            Sleep(50);
+            if(counter == 3)
+            {
+                break;
+            }
+            if (counter == 3)
+            {
+                cout << "Congratulations, you won" << endl;
+            }
+            Sleep(50);           
         }
         else
         {
@@ -169,17 +182,13 @@ void gameForEasyMode()
         }
         
     }
-    if (counter == 3)
-    {
-        cout << "Congratulations, you won" << endl;
-    }
-    else
+    if(counter != 3)
     {
         cout << "Try again to make a word!" << endl;
     }
     cout << "You lost! Go back to the Menu? Y/N: ";
     cin >> youDie;
-    if (gameOver == 'Y')
+    if (youDie == 'Y')
     {
         system("cls");
         Menu();
@@ -193,6 +202,9 @@ void gameForEasyMode()
 
 void gameForMediumMode()
 {
+    counter = 0;
+    x = 2;
+    y = 1;
     string randomMediumWord;
     char createWordMedium[5];
     string fileMedium[5];
@@ -219,19 +231,19 @@ void gameForMediumMode()
     wordMedium.close();
     //save the coordinates about the letters
     int createWordMedium1Y = { 1 + rand() % 10 };
-    int createWordMedium1X = { 1 + rand() % 25 };
+    int createWordMedium1X = { 1 + rand() % 24 };
     grid[createWordMedium1Y][createWordMedium1X] = randomMediumWord[0];
 
     int createWordMedium2Y = { 1 + rand() % 10 };
-    int createWordMedium2X = { 1 + rand() % 25 };
+    int createWordMedium2X = { 1 + rand() % 24 };
     grid[createWordMedium2Y][createWordMedium2X] = randomMediumWord[1];
 
     int createWordMedium3Y = { 1 + rand() % 10 };
-    int createWordMedium3X = { 1 + rand() % 25 };
+    int createWordMedium3X = { 1 + rand() % 24 };
     grid[createWordMedium3Y][createWordMedium3X] = randomMediumWord[2];
 
     int createWordMedium4Y = { 1 + rand() % 10 };
-    int createWordMedium4X = { 1 + rand() % 25 };
+    int createWordMedium4X = { 1 + rand() % 24 };
     grid[createWordMedium4Y][createWordMedium4X] = randomMediumWord[3];
 
     grid[createWordMedium1Y][createWordMedium1X] = randomMediumWord[0];
@@ -240,17 +252,16 @@ void gameForMediumMode()
     grid[createWordMedium4Y][createWordMedium4X] = randomMediumWord[3];
     srand(time(NULL));
 
-    while (gameOver == true)
+    while (true)
     {
-        system("cls");
-        if (y > 0 && y < 12 && x > 0 && x < 23)
+
+        if (y > 0 && y < 15 && x > 0 && x < 25)
         {
             system("cls");
             for (int map = 0; map < 13; map++)
             {
                 cout << grid[map] << endl;
             }
-
             // checking if the button is pressed
             if (GetAsyncKeyState(VK_DOWN))
             {
@@ -298,19 +309,28 @@ void gameForMediumMode()
             if (x == createWordMedium1X && y == createWordMedium1Y)
             {
                 counter++;
+                createWordMedium1X = 100;
             }
 
             if (x == createWordMedium2X && y == createWordMedium2Y)
             {
                 counter++;
+                createWordMedium2X = 100;
             }
             if (x == createWordMedium3X && y == createWordMedium3Y)
             {
                 counter++;
+                createWordMedium3X = 100;
             }
             if (x == createWordMedium4X && y == createWordMedium4Y)
             {
                 counter++;
+                createWordMedium4X = 100;
+            }
+            if (counter == 4)
+            {
+                cout << "Congratulations, you won" << endl;
+                break;
             }
             Sleep(50);
         }
@@ -334,32 +354,31 @@ void gameForMediumMode()
 
         }
 
-    }
-    if (counter == 4)
-    {
-        cout << "Congratulations, you won" << endl;
-    }
-    else
-    {
-        cout << "Try again to make a word!" << endl;
-    }
-    cout << "You lost! Go back to the Menu? Y/N: ";
-    cin >> youDie;
-    if (gameOver == 'Y')
-    {
-        system("cls");
-        Menu();
-    }
-    else
-    {
-        system("cls");
-        cout << "Ok, goodbye! Hope you liked our project!";
+        if (counter != 4)
+        {
+            cout << "Try again to make a word!" << endl;
+        }
+        cout << "You lost! Go back to the Menu? Y/N: ";
+        cin >> youDie;
+        if (youDie == 'Y')
+        {
+            system("cls");
+            Menu();
+        }
+        else
+        {
+            system("cls");
+            cout << "Ok, goodbye! Hope you liked our project!";
+        }
     }
 }
 
 
 void gameForHardMode()
 {
+    counter = 0;
+    x = 2;
+    y = 1;
     string randomHardWord;
     string fileHard[5];
     string word;
@@ -385,23 +404,23 @@ void gameForHardMode()
     srand(time(NULL));
     //save the coordinates about the letters
     int createWordHard1Y = { 1 + rand() % 10 };
-    int createWordHard1X = { 1 + rand() % 25 };
+    int createWordHard1X = { 1 + rand() % 24 };
     grid[createWordHard1Y][createWordHard1X] = randomHardWord[0];
 
     int createWordHard2Y = { 1 + rand() % 10 };
-    int createWordHard2X = { 1 + rand() % 25 };
+    int createWordHard2X = { 1 + rand() % 24 };
     grid[createWordHard2Y][createWordHard2X] = randomHardWord[1];
     
     int createWordHard3Y = { 1 + rand() % 10 };
-    int createWordHard3X = { 1 + rand() % 25 };
+    int createWordHard3X = { 1 + rand() % 24 };
     grid[createWordHard3Y][createWordHard3X] = randomHardWord[2];
 
     int createWordHard4Y = { 1 + rand() % 10 };
-    int createWordHard4X = { 1 + rand() % 25 };
+    int createWordHard4X = { 1 + rand() % 24 };
     grid[createWordHard4Y][createWordHard4X] = randomHardWord[3];
 
     int createWordHard5Y = { 1 + rand() % 10 };
-    int createWordHard5X = { 1 + rand() % 25 };
+    int createWordHard5X = { 1 + rand() % 24 };
     grid[createWordHard5Y][createWordHard5X] = randomHardWord[4];
 
     grid[createWordHard1Y][createWordHard1X] = randomHardWord[0];
@@ -416,7 +435,7 @@ void gameForHardMode()
     grid[1 + rand() % 10][1 + rand() % 25] = randomHardWord[3];
     grid[1 + rand() % 10][1 + rand() % 25] = randomHardWord[4];
     system("cls");
-    while (gameOver = true)
+    while (true)
     {
         system("cls");
         if (y > 0 && y < 12 && x > 0 && x < 23)
@@ -481,23 +500,33 @@ void gameForHardMode()
             if (x == createWordHard1X && y == createWordHard1Y)
             {
                 counter++;
+                createWordHard1X = 100;
             }
 
             if (x == createWordHard2X && y == createWordHard2Y)
             {
                 counter++;
+                createWordHard2X = 100;
             }
             if (x == createWordHard3X && y == createWordHard3Y)
             {
                 counter++;
+                createWordHard3X = 100;
             }
             if (x == createWordHard4X && y == createWordHard4Y)
             {
                 counter++;
+                createWordHard4X = 100;
             }
             if (x == createWordHard5X && y == createWordHard5Y)
             {
                 counter++;
+                createWordHard5X = 100;
+            }
+            if (counter == 5)
+            {
+                break;
+                cout << "Congratulations, you won" << endl;
             }
             Sleep(50);
         }
@@ -522,15 +551,10 @@ void gameForHardMode()
         }
 
     }
-    if (counter == 5)
-    {
-        cout << "Congratulations, you won" << endl;
-    }
-    else
+    if(counter != 5)
     {
         cout << "Try again to make a word!" << endl;
     }
-    cout << "The correct word was: " << word;
     cout << "You lost! Go back to the Menu? Y/N: ";
     cin >> youDie;
     if (gameOver == 'Y')
